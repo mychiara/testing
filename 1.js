a2e8f6d8ce05362ad8a8a12eb2e2f7ec99e36128
mychiara <85479169+mychiara@users.noreply.github.com>
2024-06-10 21:48:28 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '493b82e09d8ace8a2fbdf3b012221754',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/493b82e09d8ace8a2fbdf3b012221754/invoke.js"></scr' + 'ipt>')
